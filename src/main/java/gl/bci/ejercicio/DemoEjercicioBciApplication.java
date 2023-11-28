68db3c3629a65e7d85b3d4629832c7c01c49cc44
omar.rebollo <omar.rebollo@gmail.com>
2023-11-28 11:29:14 -0600

    First commit

---
+++ src/main/java/gl/bci/ejercicio/DemoEjercicioBciApplication.java
@@ -0,0 +1,13 @@
+package gl.bci.ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEjercicioBciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEjercicioBciApplication.class, args);
+	}
+
+}
